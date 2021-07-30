dd4e6c1670196c07d088e52b99d395c5aed85420
chika <983123963@qq.com>
2021-07-30 19:32:59 +0800

    feat(loadingBar): 增加加载条与主题颜色适配

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -138,6 +138,7 @@
         window.addEventListener('resize', watchWidth);
         //挂载在 window 方便与在js中使用
         window['$loading'] = useLoadingBar();
+        window['$loading'].finish();
       });
 
       return {
