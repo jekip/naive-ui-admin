bceb0dc6e23165ad259843c30dbc0c9b4cb9fefd
eGluZl <im.hxy@outlook.com>
2021-10-27 11:16:17 +0800

    fix: 修正generateRoutes方法不能正常过滤权限的问题。（是把此处的item当作ref了？）

--- src/store/modules/asyncRoute.ts
+++ src/store/modules/asyncRoute.ts
@@ -93,7 +93,7 @@ export const useAsyncRouteStore = defineStore({
         const { meta } = route;
         const { permissions } = meta || {};
         if (!permissions) return true;
-        return permissionsList.some((item) => permissions.includes(item.value));
+        return permissionsList.some((item) => permissions.includes(item));
       };
       const { getPermissionMode } = useProjectSetting();
       const permissionMode = unref(getPermissionMode);
