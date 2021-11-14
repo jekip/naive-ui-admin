4f81743f9038f08a41d7bd963385307273d2b8b5
litao <897262216@qq.com>
2021-11-14 10:28:54 +0800

    按需引入NaiveUi并自动创建.d.ts声明，在volar中使用组件可获得代码提示和类型检查

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -1,4 +1,6 @@
 import type { Plugin } from 'vite';
+import Components from 'unplugin-vue-components/vite';
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
 
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
@@ -15,6 +17,12 @@ export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean, prodMock) 
     vue(),
     // have to
     vueJsx(),
+
+    // 按需引入NaiveUi且自动创建组件声明
+    Components({
+      dts: true,
+      resolvers: [NaiveUiResolver()],
+    }),
   ];
 
   // vite-plugin-html
