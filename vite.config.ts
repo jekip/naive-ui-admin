00247ee7b9f8114ade8e2bfafc9ef35d960d7031
ahjung <735878602@qq.com>
2025-08-12 16:40:20 +0800

    调整构建`target`

--- vite.config.ts
+++ vite.config.ts
@@ -56,7 +56,7 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
       exclude: ['vue-demi'],
     },
     build: {
-      target: 'es2015',
+      target: 'es2020',
       cssTarget: 'chrome80',
       outDir: OUTPUT_DIR,
       reportCompressedSize: false,
